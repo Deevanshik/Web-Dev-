b19a867809ee5eeead662a05b658d23064fd1c56
Deevanshik <deevanshikarora484@gmail.com>
2026-01-26 09:01:25 +0530

    Basics of node done

---
+++ Phase-1/nodejs-crash-course/index.js
@@ -0,0 +1,5 @@
+import getPosts, { getPostLength } from "./postController.js";
+
+console.log(getPosts());
+console.log(getPostLength());
+
---
+++ Phase-1/nodejs-crash-course/postController.js
@@ -0,0 +1,12 @@
+const posts = [
+  { id: 1, title: "One" },
+  { id: 2, title: "Two" },
+  { id: 3, title: "Three" },
+];
+
+const getPosts = () => posts;
+
+const getPostLength = () => posts.length;
+
+export { getPostLength };
+export default getPosts;
